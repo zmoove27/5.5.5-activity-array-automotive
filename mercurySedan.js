ee6ee9e37497a00309b99f1df33db1786ea5b4f6
[Zmoove27] <[zoren.villareal@gmail.com]>
2023-05-09 15:49:12 -0700

    finished

--- mercurySedan.js
+++ mercurySedan.js
@@ -2,10 +2,49 @@
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+// let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
 
+class Car extends Vehicle {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color,mileage);
+    this.maxPassenger = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maximumSpeed = 160;
+    this.fuel =10
+    this
 
+  }
+  heckService() {
+    if (this.mileage > 30000) {
+        this.scheduleService = true
+        return this.scheduleService;
+    }
+}
+start() {
+    if (this.fuel > 0) {
+        console.log("engine has started.");
+        return this.started = true
+    } else {
+        console.log("no fuel");
+        return this.started = false;
+      }
+  }
+  loadPassenger(num) {
+      if (this.passenger < this.maxPassengers) {
+          if ((num + this.passenger) <= this.maxPassengers) {
+              this.passenger = num;
+              return this.passenger;
+          } else {
+              console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+          }
+      } else {
+          console.log(this.model + " " + this.make + " is full");
+      }
+  }
+
+}
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
